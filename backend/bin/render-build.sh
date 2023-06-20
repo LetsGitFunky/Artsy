3bd983b09eb9276df8ab4dc7977ca330ab954f38
LetsGitFunky <NickHeinBass@gmail.com>
2023-06-20 10:38:37 -0700

    renderrrrrrr

--- backend/bin/render-build.sh
+++ backend/bin/render-build.sh
@@ -3,7 +3,12 @@
 # exit on error
 set -o errexit
 
-npm run build
+# Build the React app
+npm install --prefix frontend
+npm run build --prefix frontend
+
+# Install Rails dependencies and migrate the database
 bundle install
 rails db:migrate
-rails db:seed #if needed
+# Uncomment the following line if you want to seed the database on every build
+# rails db:seed
