aa66a3e3187548c07ee1371bd7acd49457f78523
LetsGitFunky <NickHeinBass@gmail.com>
2023-06-20 11:51:45 -0700

    render reset

--- backend/bin/render-build.sh
+++ backend/bin/render-build.sh
@@ -1,14 +1,26 @@
 #!/usr/bin/env bash
 
 # exit on error
-set -o errexit
+# set -o errexit
 
 # Build the React app
-npm install --prefix backend/frontend
-npm run build --prefix backend/frontend
+# npm install --prefix backend/frontend
+# npm run build --prefix backend/frontend
 
 # Install Rails dependencies and migrate the database
-cd backend && bundle install
-cd backend && rails db:migrate
+# cd backend && bundle install
+# cd backend && rails db:migrate
 # Uncomment the following line if you want to seed the database on every build
 # rails db:seed
+
+# //////////////////////////////////////////////////////////////////////
+
+#!/usr/bin/env bash
+
+# exit on error
+set -o errexit
+
+npm run build
+bundle install
+rails db:migrate
+rails db:seed #if needed
