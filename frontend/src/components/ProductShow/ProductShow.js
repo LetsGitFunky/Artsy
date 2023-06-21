0afbb3c6da96c8949b58c18addfeec3ad23b967a
LetsGitFunky <NickHeinBass@gmail.com>
2023-06-21 16:03:37 -0700

    img carousel and Product Show dial

--- frontend/src/components/ProductShow/ProductShow.js
+++ frontend/src/components/ProductShow/ProductShow.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchProduct } from '../../store/product';
 import { useParams } from 'react-router-dom';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // This imports the styles
 import "./ProductShow.css"
 import "../SessionFormPage/FormStyles.css"
 
@@ -27,42 +29,42 @@ function ProductShow() {
         setSelectedSize(e.target.value);
     }
 
-    const handleThumbnailClick = (index) => {
-        setCurrentImgIndex(index);
-    }
-
-    const handlePrevClick = () => {
-        setCurrentImgIndex((currentImgIndex - 1 + product.img_urls.length) % product.img_urls.length);
-    }
-
-    const handleNextClick = () => {
-        setCurrentImgIndex((currentImgIndex + 1) % product.img_urls.length);
-    }
-
     return (
         <div className="prod-show-container">
-        <div className="prod-img-container">
-        <div className="thumbnails-container">
-            {product.img_urls.map((url, index) => (
-                <img
-                    className={`thumbnail ${index === currentImgIndex ? "selected" : ""}`}
-                    key={index}
-                    src={url}
-                    alt={`${product.name} ${index}`}
-                    onClick={() => setCurrentImgIndex(index)}
-                />
-            ))}
-        </div>
-            <button onClick={handlePrevClick}>Prev</button>
 
-            <img
-            className="prod-main-img"
-            // src={product.img_urls ? product.img_urls[currentImgIndex] : "https://shorturl.at/wJKQY"}
-            src={product.img_urls[currentImgIndex]}
-            alt={product.name}
-            />
 
-            <button onClick={handleNextClick}>Next</button>
+        <div className="img-carousel-container">
+            <div className="thumbnails-container">
+                {product.img_urls.map((url, index) => (
+                    <img
+                        className={`thumbnail ${index === currentImgIndex ? "selected" : ""}`}
+                        key={index}
+                        src={url}
+                        alt={`${product.name} ${index}`}
+                        onClick={() => setCurrentImgIndex(index)}
+                    />
+                ))}
+            </div>
+
+            <div className="prod-img-container">
+                <Carousel
+                    selectedItem={currentImgIndex}
+                    onChange={setCurrentImgIndex}
+                    showThumbs={false}
+                    autoPlay={false}
+                    infiniteLoop={true}
+                    showIndicators={false}
+                >
+                    {product.img_urls.map((url, index) => (
+                        <div key={index}>
+                            <img
+                            className="carousel-main-img"
+                            src={url}
+                            alt={`${product.name} ${index}`} />
+                        </div>
+                    ))}
+                </Carousel>
+            </div>
         </div>
 
         <div className="prod-info-container">
@@ -76,7 +78,7 @@ function ProductShow() {
                 <option value="">Select size</option>
                 {product.sizes.map((size, index) => (
                     <option key={index} value={size}>{size}</option>
-                ))}
+                    ))}
             </select>
 
             <button className='formButton'>Add to Cart</button>
@@ -85,4 +87,41 @@ function ProductShow() {
     );
 }
 
+
+// const handleThumbnailClick = (index) => {
+//     setCurrentImgIndex(index);
+// }
+
+// const handlePrevClick = () => {
+//     setCurrentImgIndex((currentImgIndex - 1 + product.img_urls.length) % product.img_urls.length);
+// }
+
+// const handleNextClick = () => {
+//     setCurrentImgIndex((currentImgIndex + 1) % product.img_urls.length);
+// }
+
+{/* <div className="prod-img-container">
+<div className="thumbnails-container">
+    {product.img_urls.map((url, index) => (
+        <img
+            className={`thumbnail ${index === currentImgIndex ? "selected" : ""}`}
+            key={index}
+            src={url}
+            alt={`${product.name} ${index}`}
+            onClick={() => setCurrentImgIndex(index)}
+        />
+    ))}
+</div>
+
+    <button onClick={handlePrevClick}>Prev</button>
+
+    <img
+    className="prod-main-img"
+    // src={product.img_urls ? product.img_urls[currentImgIndex] : "https://shorturl.at/wJKQY"}
+    src={product.img_urls[currentImgIndex]}
+    alt={product.name}
+    />
+
+    <button onClick={handleNextClick}>Next</button>
+</div> */}
 export default ProductShow;
