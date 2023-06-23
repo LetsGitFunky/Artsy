92d8b666784649f737db6e992673622ec6309980
LetsGitFunky <NickHeinBass@gmail.com>
2023-06-22 17:58:07 -0700

    started review styling

--- frontend/src/components/Reviews/ReviewIndexItem/ReviewIndexItem.js
+++ frontend/src/components/Reviews/ReviewIndexItem/ReviewIndexItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./ReviewIndexItem.css";
+import {FaThumbsUp} from "react-icons/fa"
 
 const ReviewIndexItem = ({review}) => {
     return (
@@ -7,7 +8,8 @@ const ReviewIndexItem = ({review}) => {
             <p className="rii-rating">Rating: {review.rating}</p>
             <p className="rii-title rii-text">{review.title}</p>
             <p className="rii-body rii-text">{review.body}</p>
-            <button className="rii-helpful rii-text">Helpful?</button>
+            <p className="rii-author rii-text">Reviewed by: {review.author}</p>
+            <button className="rii-helpful rii-text"><FaThumbsUp size={10}/> Helpful?</button>
             {/* You might want to add more details, such as the review author's name, etc. */}
         </div>
     )
