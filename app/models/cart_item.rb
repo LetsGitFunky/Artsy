fd7a0533650e98f8ec46d1d4a5e86c234f458786
LetsGitFunky <NickHeinBass@gmail.com>
2023-06-27 10:18:29 -0700

    cartitems backend

---
+++ app/models/cart_item.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: cart_items
+#
+#  id         :bigint           not null, primary key
+#  user_id    :bigint           not null
+#  product_id :bigint           not null
+#  quantity   :integer          not null
+#  options    :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class CartItem < ApplicationRecord
+    belongs_to :user
+    belongs_to :product
+
+    validates :quantity, :options, :product_id, :user_id, presence: true
+    validates :quanitiy, numericality: { only_integer: true, greater_than: 0 }
+
+end
