92ec642a05e6efb5be2853d6b967970ff1ef3df7
LetsGitFunky <NickHeinBass@gmail.com>
2023-06-28 14:12:04 -0700

    existing cart item check

--- frontend/src/components/Cart/AddToCartButton/AddToCartButton.jsx
+++ frontend/src/components/Cart/AddToCartButton/AddToCartButton.jsx
@@ -1,22 +1,34 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { createCartItem } from "../../../store/cart_item";
+import { createCartItem, updateCartItem } from "../../../store/cart_item";
 import "./AddToCartButton.css"
 
 const AddToCartButton = ({ product, selectedSize }) => {
-    const dispatch = useDispatch()
-    const currentUser = useSelector(state => state.session.user)
+    const dispatch = useDispatch();
+    const currentUser = useSelector(state => state.session.user);
+    const cartItems = useSelector(state => state.cartItems);
 
     const [quantity, setQuantity] = useState(1)
-    // const quantity = 1;
 
     const handleAddToCart = () => {
-        dispatch(createCartItem({
-            user_id: currentUser.id,
-            product_id: product.id,
-            quantity: quantity,
-            options: selectedSize
-        }))
+
+        const existingCartItem = Object.values(cartItems).find(
+            item => item.productId === product.id && item.options === selectedSize
+        );
+
+        if (existingCartItem) {
+            dispatch(updateCartItem({
+                ...existingCartItem,
+                quantity: existingCartItem.quantity + 1
+            }));
+        } else {
+            dispatch(createCartItem({
+                user_id: currentUser.id,
+                product_id: product.id,
+                quantity: quantity,
+                options: selectedSize
+            }))
+        }
     }
 
     return (
