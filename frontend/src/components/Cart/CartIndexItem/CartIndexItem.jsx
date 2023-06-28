5c64aedd80b26f426395c762b473764262bd2b7b
LetsGitFunky <NickHeinBass@gmail.com>
2023-06-28 15:12:49 -0700

    styling cart

--- frontend/src/components/Cart/CartIndexItem/CartIndexItem.jsx
+++ frontend/src/components/Cart/CartIndexItem/CartIndexItem.jsx
@@ -32,23 +32,29 @@ const CartIndexItem = ({ cartItem }) => {
         <div className="cart-item">
             {/* <div className="cart-item-productId">ProductId: {cartItem.productId}</div> */}
             <div className="cart-prod-name">{cartItem.productName}</div>
-            <img className="cart-prod-img" src={cartItem.productImage} alt={cartItem.productName} />
-            <div className="cart-prod-price">${cartItem.productPrice}.00</div>
-            <div className="cart-item-options">Options: {cartItem.options}</div>
-            <div className="cart-item-quantity">
-                <label className='quantity-label'>Quantity
-                    <select value={quantity} onChange={handleQuantityChange}>
-                        <option value={1}>1</option>
-                        <option value={2}>2</option>
-                        <option value={3}>3</option>
-                        <option value={4}>4</option>
-                        <option value={5}>5</option>
-                        <option value={6}>6</option>
-                        <option value={7}>7</option>
-                    </select>
-                </label>
+            <div className='cart-prod-container'>
+                <img className="cart-prod-img" src={cartItem.productImage} alt={cartItem.productName} />
+                <div className='cart-prod-text-container'>
+                    <div className="cart-item-options">Options: {cartItem.options}</div>
+                        <div className="cart-item-quantity">
+                            <label className='quantity-label'>Quantity
+                                <select className='quantity-select' value={quantity} onChange={handleQuantityChange}>
+                                    <option value={1}>1</option>
+                                    <option value={2}>2</option>
+                                    <option value={3}>3</option>
+                                    <option value={4}>4</option>
+                                    <option value={5}>5</option>
+                                    <option value={6}>6</option>
+                                    <option value={7}>7</option>
+                                </select>
+                            </label>
+                        </div>
+                </div>
+            <div className='cart-price-container'>
+                <div className="cart-item-total-price">${totalItemPrice}.00</div>
+                <div className="cart-prod-price">(${cartItem.productPrice}.00 each)</div>
             </div>
-            <div className="cart-item-total-price">Total: ${totalItemPrice}.00</div>
+            </div>
             <button className="remove-item-button" onClick={handleRemoveItem}><AiOutlineClose/> Remove Item</button>
         </div>
     )
